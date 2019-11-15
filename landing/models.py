23ac4e2dcb2e78b2feb7a7531ba56a6d8cc932f6
arieltev <arotev@gmail.com>
2019-11-15 11:02:17 +0200

    seting up db sqlite3

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Developer(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.name
