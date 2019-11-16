17b0c8c65c346a0936f59d3e26408af9329ca0fd
arieltev <arotev@gmail.com>
2019-11-16 09:59:24 +0200

    an updated commit message

--- landingsite/settings.py
+++ landingsite/settings.py
@@ -74,13 +74,44 @@ WSGI_APPLICATION = 'landingsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '/cloudsql/tevdevelops:europe-west2:tevinstance',
+        'NAME':'Developers',
+        'USER':'admin',
+        'PASSWORD':'admin123',
     }
-}
-
+} """
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '/cloudsql/tevdevelops:europe-west2:tevinstance',
+            'USER': 'admin',
+            'PASSWORD': 'admin123',
+            'NAME': 'Developers',
+        }
+    }
+else:
+    # Running locally so connect to either a local MySQL instance or connect to
+    # Cloud SQL via the proxy. To start the proxy via command line:
+    #
+    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    #
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'NAME': 'Developers',
+            'USER': 'admin',
+            'PASSWORD': 'admin123',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -118,8 +149,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'https://storage.googleapis.com/tevstudio-static/static/'
+# STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static', 'tevstudio-static')
+
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'landingsite/static')
 ]
